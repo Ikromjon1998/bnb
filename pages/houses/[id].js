32d8025571e9d8512530133f80c9dbfc94e93d75
Ikromjon Ochilov <s288560@studenti.polito.it>
2021-12-15 19:42:25 +0100

    errors is fixed

--- pages/houses/[id].js
+++ pages/houses/[id].js
@@ -1,20 +1,21 @@
 import Head from "next/head"
 import houses from "../../houses.js"
-import Layout from '../components/Layout'
+import Layout from "../../components/Layout"
 
 export default function House(props) {
   return (
-    <Layout content = {
-      <div>
-        <Head>
-          <title>{props.house.title}</title>
-        </Head>
-        <img src={props.house.picture} width="100%" alt="House picture" />
-        <p>
-          {props.house.type} - {props.house.town}
-        </p>
-        <p>{props.house.title}</p>
-      </div>
+    <Layout
+      content={
+        <div>
+          <Head>
+            <title>{props.house.title}</title>
+          </Head>
+          <img src={props.house.picture} width="100%" alt="House picture" />
+          <p>
+            {props.house.type} - {props.house.town}
+          </p>
+          <p>{props.house.title}</p>
+        </div>
       }
     />
   )
