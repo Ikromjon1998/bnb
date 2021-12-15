78b3b4469c6e70306653721e52fd8b94d66bccd8
Ikromjon Ochilov <s288560@studenti.polito.it>
2021-12-15 19:12:38 +0100

    third-step

--- components/House.js
+++ components/House.js
@@ -1,15 +1,15 @@
-export default function House(props) {
-    
-    return (
-        <div>
-            <img src={props.picture} width="100%" alt="House picture" />
+import Link from "next/link"
 
-            <p>
-                {props.type} - {props.town}
-            </p>
-            <p>
-                {props.title}
-            </p>
-        </div>
-    )
+export default function House(props) {
+  return (
+    <Link href="/houses/[id]" as={"/houses/" + props.id}>
+      <a>
+        <img src={props.picture} width="100%" alt="House picture" />
+        <p>
+          {props.type} - {props.town}
+        </p>
+        <p>{props.title}</p>
+      </a>
+    </Link>
+  )
 }
