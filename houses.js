fadf8ea89afa1f66d7af5892c77ff76e61da7b5b
Ikromjon Ochilov <s288560@studenti.polito.it>
2021-12-15 17:56:30 +0100

    First step

---
+++ houses.js
@@ -0,0 +1,15 @@
+
+export default [
+    {
+      picture: '/img/bedroom.jpg',
+      type: 'Entire house',
+      town: 'New York',
+      title: 'Beautiful flat in New York!'
+    },
+    {
+      picture: '/img/main.jpg',
+      type: 'Entire house',
+      town: 'Amsterdam',
+      title: 'A flat in Amsterdam with a great view'
+    }
+]
